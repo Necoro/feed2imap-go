b2f7bcf657790931411bda4fd2d0a9418aec8489
René 'Necoro' Neumann <necoro@necoro.eu>
2020-04-18 18:04:33 +0200

    Move to yaml_test

--- internal/config/yaml_test.go
+++ internal/config/yaml_test.go
@@ -5,32 +5,6 @@ import (
 	"testing"
 )
 
-func TestLoad(t *testing.T) {
-	type args struct {
-		path string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    Config
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := Load(tt.args.path)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Load() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Load() got = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func s(s string) *string { return &s }
 func b(b bool) *bool     { return &b }
 
