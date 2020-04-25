ff1f2be30896a7c01517805b7ad3473f5b4493fc
René 'Necoro' Neumann <necoro@necoro.eu>
2020-04-25 17:03:28 +0200

    Remove Feeds.String
    With using `spew` this is no longer needed

--- pkg/config/feed.go
+++ pkg/config/feed.go
@@ -1,10 +1,5 @@
 package config
 
-import (
-	"fmt"
-	"strings"
-)
-
 // One stored feed
 type Feed struct {
 	Name    string
@@ -15,24 +10,3 @@ type Feed struct {
 
 // Convenience type for all feeds
 type Feeds map[string]Feed
-
-func (feeds Feeds) String() string {
-	var b strings.Builder
-	app := func(a ...interface{}) {
-		_, _ = fmt.Fprint(&b, a...)
-	}
-	app("Feeds [")
-
-	first := true
-	for k, v := range feeds {
-		if !first {
-			app(", ")
-		}
-		app(`"`, k, `"`, ": ")
-		_, _ = fmt.Fprintf(&b, "%+v", v)
-		first = false
-	}
-	app("]")
-
-	return b.String()
-}
