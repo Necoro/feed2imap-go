5a169ab960333b28e3d0d6a72fc4bfaf67a00354
René 'Necoro' Neumann <necoro@necoro.eu>
2020-05-04 15:43:25 +0200

    Increase version

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 // the way via debug.BuildInfo does not work -- it'll always return "devel"
 // thus the oldschool way: hardcoded
 
-const version = "0.1.0"
+const version = "0.2.0-devel"
 
 // Current feed2imap version
 func Version() string {
